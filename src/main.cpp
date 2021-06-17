14d41357203533badf3721743df82dafcf0efebb
Ibrahim Abdulsamad <abdulsamadibrahim210@gmail.com>
2021-06-17 20:15:42 +0100

    fixes runtime error

--- src/main.cpp
+++ src/main.cpp
@@ -12,34 +12,40 @@
 
 int main(int argc, const char* argv[]) {
 
-    const auto path = argc == 2 ? std::filesystem::path { argv[1] } : std::filesystem::current_path();
+    try {
+        const auto path = argc == 2 ? std::filesystem::path { argv[1] } : std::filesystem::current_path();
 
-    std::filesystem::directory_entry base_entry_dir(path);
+        std::filesystem::directory_entry base_entry_dir(path);
 
-    std::string file_name = "";
+        std::string file_name = "";
 
-    uintmax_t file_size = 0;
+        uintmax_t file_size = 0;
 
-    auto cftime = std::chrono::system_clock::to_time_t(std::chrono::file_clock::to_sys(base_entry_dir.last_write_time()));
+        auto cftime = std::chrono::system_clock::to_time_t(std::chrono::file_clock::to_sys(base_entry_dir.last_write_time()));
 
-    std::string last_modified = std::asctime(std::localtime(&cftime));
+        std::string last_modified = std::asctime(std::localtime(&cftime));
 
-    FileType file_type = FileType::file;
+        FileType file_type = FileType::file;
 
-    if(base_entry_dir.is_directory()) {
+        if(base_entry_dir.is_directory()) {
+            
+            file_type = FileType::directory;
+        }else {
+            file_size = base_entry_dir.file_size();
+
         
-        file_type = FileType::directory;
-    }else {
-        file_size = base_entry_dir.file_size();
+        }
 
-    
+        FileStructure base_structure(file_name,file_type,last_modified,file_size, path); 
+
+        base_structure.add_children(path);
+
+        base_structure.print_structure(0);
+    }catch(const std::runtime_error& error) {
+        std::cerr << error.what();
     }
 
-    FileStructure base_structure(file_name,file_type,last_modified,file_size, path); 
-
-    base_structure.add_children(path);
-
-    base_structure.print_structure(0);
+    
 
    
        
